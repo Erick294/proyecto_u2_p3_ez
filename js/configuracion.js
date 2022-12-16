bad9112cded84874f78d952bf448ae6037db3b9b
Erick294 <beowulfblackman@hotmail.com>
2022-12-15 19:45:31 -0500

    Taller 12

--- js/configuracion.js
+++ js/configuracion.js
@@ -1,10 +1,28 @@
 console.log(Vue)
 
+//Options API
 const app = Vue.createApp({
-    template: `
-    <h2>Hola mundo!</h2>
-    {{1+1}}
-    `
+    //template: `
+    //<h2>Hola mundo!</h2>
+    //<p>{{1+1}}</p>
+    //`
+    data(){
+        //propiedades reactivas: datos que usamos dentro de la pagina web
+        return{
+            mensaje: 'Hola mundo',
+            nombre: 'Erick',
+            apellido: 'Zambrano'
+        }
+    },
+    methods:{
+        cambiarNombre(event){
+            this.nombre = "Benito"
+            console.log(event)
+        },
+        cambiarApellido(){
+            this.apellido = "Juarez"
+        }
+    }
 })
 
 app.mount('#myApp')
